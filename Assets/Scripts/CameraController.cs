11ead749de7b591717847cd88fa380ea5e4fa2f7
Philip <vietwhammies@gmail.com>
2019-09-09 23:06:45 -0700

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // For follow cameras, procedural animation gathering last
+    // known states, use LateUpdate, used like Update but it is
+    // guaranteed to run after all items have been processed in Update
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
